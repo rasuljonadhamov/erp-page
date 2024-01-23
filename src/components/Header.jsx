c6aea1b7487de131d3f1e191422053c1a01d2fe4
rasuljonadhamov <rasuljonadhamov@gmail.com>
2024-01-23 22:13:47 +0500

    created header , sidebar and content components

---
+++ src/components/Header.jsx
@@ -0,0 +1,56 @@
+import { Button, ConfigProvider, Menu } from "antd";
+import { Header } from "antd/es/layout/layout";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+
+const MyHeader = ({ collapsed, setCollapsed }) => {
+  return (
+    <Header
+      style={{
+        padding: 0,
+        position: "sticky",
+        fontSize: "16px",
+        top: 0,
+        zIndex: 1,
+        display: "flex",
+        alignItems: "center",
+        gap: "1140px",
+        justifyContent: "space-between",
+      }}
+    >
+      <Button
+        type="text"
+        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+        onClick={() => setCollapsed(!collapsed)}
+        style={{
+          width: 64,
+          height: 64,
+          color: "white",
+          marginRight: "auto",
+        }}
+      />
+
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={["1"]}
+        items={[
+          {
+            key: 1,
+            label: `Chiqish `,
+            icon: <LogoutOutlined />,
+          },
+        ]}
+        style={{
+          flex: 1,
+          minWidth: 0,
+        }}
+      />
+    </Header>
+  );
+};
+
+export default MyHeader;
