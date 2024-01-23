57caf253130abdd7bfa495a20e93b78c1fdfe769
rasuljonadhamov <rasuljonadhamov@gmail.com>
2024-01-23 19:55:47 +0500

    created root component and styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { ConfigProvider } from "antd";
+import Root from "./pages/Root";
 
 const App = () => {
   return (
     <div>
-      
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#bc8e5c",
+          },
+        }}
+      >
+        <Root />
+      </ConfigProvider>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
